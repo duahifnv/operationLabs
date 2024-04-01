203ecb7873306e84e615c2ca882800220b231fc7
duahifnv <max135603@gmail.com>
2024-04-01 13:51:13 +0300

    Добавлен метод создания исходного опорного плана (рекурсионное распределение)

--- TransportProblem/src/Main.java
+++ TransportProblem/src/Main.java
@@ -2,11 +2,13 @@ import transport.*;
 
  class Main {
     public static void main(String[] args) {
-        int[] srcWeights = {200, 120, 190};
+        int[] srcWeights = {200, 120, 150};
         int[] dstWeights = {100, 90, 200, 30, 80};
         int[][] costs = {{1, 2, 4, 1, 5},
                          {1, 2, 1, 3, 1},
                          {2, 1, 3, 3, 1}};
         Transport transport = new Transport(srcWeights, dstWeights, costs);
+        transport.CreateSourcePlan();
+            transport.PrintTable();
     }
 }
--- TransportProblem/src/transport/Transport.java
+++ TransportProblem/src/transport/Transport.java
@@ -1,8 +1,12 @@
 package transport;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
 
+import utils.Table;
 /**
  * Класс для решения транспортной задачи
  */
@@ -68,9 +72,46 @@ public class Transport {
             }
         }
     }
-
-    // TODO: Опорный план (метод СЗ)
-    /* private void CreateSourcePlan() {
-
-    }*/
+    public void PrintTable() {
+        List<String> labels = IntStream.range(0, dstWeigths.length)
+                .mapToObj(i -> "B" + (i + 1) + " / " + dstWeigths[i]).toList();
+        List<List<String>> params = new ArrayList<>();
+        for (int i = 0; i < srcWeigths.length; i++) {
+            List<String> row = new ArrayList<>(Collections.singleton("A" + (i + 1) + " / " + srcWeigths[i]));
+            for (Cell cell : table[i]) {
+                String value = (cell.isHasTraffic()) ? String.valueOf(cell.getTraffic()) : "-";
+                row.add(value + "|" + cell.getCost());
+            }
+            params.add(row);
+        }
+        Table.PrintTable(dstWeigths.length, labels, params,
+                "Исходный опорный план", Collections.nCopies(dstWeigths.length, 10));
+    }
+    /** Создание исходного опорного плана задачи */
+    public void CreateSourcePlan() {
+        int[] srcTemp = Arrays.copyOf(srcWeigths, srcWeigths.length);
+        int[] dstTemp = Arrays.copyOf(dstWeigths, dstWeigths.length);
+        CellDistribute(srcTemp, dstTemp, 0, 0);
+    }
+    /** Рекурсивная функция распределения поставок по ячейкам */
+    public void CellDistribute(int[] srcTemp, int[] dstTemp, int src, int dst) {
+        if (src == srcTemp.length || dst == dstTemp.length) {
+            if (srcTemp[src - 1] != 0 || dstTemp[dst - 1] != 0) {
+                throw new Error("Невозможно составить исходный план методом СЗ");
+            }
+            return;
+        }
+        if (srcTemp[src] >= dstTemp[dst]) {
+            table[src][dst].setTraffic(dstTemp[dst]);
+            srcTemp[src] -= dstTemp[dst];
+            dstTemp[dst] = 0;
+            CellDistribute(srcTemp, dstTemp, src, dst + 1); // У поставщика еще остался груз
+        }
+        else {
+            table[src][dst].setTraffic(srcTemp[src]);
+            dstTemp[dst] -= srcTemp[src];
+            srcTemp[src] = 0;
+            CellDistribute(srcTemp, dstTemp, src + 1, dst); // Потребителю нужно еще груза
+        }
+    }
 }
