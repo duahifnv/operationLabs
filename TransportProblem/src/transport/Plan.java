6696c684733b73fa4cce9eeec94f4f5b033c08f7
duahifnv <max135603@gmail.com>
2024-04-02 00:07:48 +0300

    Рефакторинг: удалено наследование класса опорного плана от таблицы плана

--- TransportProblem/src/transport/Plan.java
+++ TransportProblem/src/transport/Plan.java
@@ -1,52 +1,33 @@
 package transport;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  *  Класс опорного плана
  */
-class Plan extends Table{
-    private Cell[][] plan;
+public class Plan {
+    private Table table;
+    private List<Integer> base;
+    private int totalCost;
     /** Конструктор опорного плана */
-    public Plan(int[] srcWeights, int[] dstWeights, int[][] costs, Cell[][] table) {
-        super(srcWeights, dstWeights, costs);
-        this.plan = table;
+    public Plan(Table table, List<Integer> base, int totalCost) {
+        this.table = table;
+        this.base = base;
+        this.totalCost = totalCost;
     }
-    /** Создание исходного опорного плана задачи */
-    public void CreateSourcePlan() {
-        int[] srcTemp = Arrays.copyOf(srcWeights, srcWeights.length);
-        int[] dstTemp = Arrays.copyOf(dstWeights, dstWeights.length);
-        CellDistribute(srcTemp, dstTemp, 0, 0);
+    public Table getTable() {
+        return table;
     }
-    /** Рекурсивная функция распределения поставок по ячейкам */
-    private void CellDistribute(int[] srcTemp, int[] dstTemp, int src, int dst) {
-        // Вышли за пределы таблицы, но груз не распределен
-        if (src == srcTemp.length || dst == dstTemp.length) {
-            throw new Error("Невозможно распределить груз методом СЗ");
-        }
-        // У поставщика еще остался груз
-        if (srcTemp[src] > dstTemp[dst]) {
-            plan[src][dst].setTraffic(dstTemp[dst]);
-            srcTemp[src] -= dstTemp[dst];
-            dstTemp[dst] = 0;
-            CellDistribute(srcTemp, dstTemp, src, dst + 1);
-        }
-        // У поставщика и потребителя удовлетворены потребности
-        else if (srcTemp[src] == dstTemp[dst]) {
-            plan[src][dst].setTraffic(dstTemp[dst]);
-            srcTemp[src] = dstTemp[dst] = 0;
-            // Конец распределния
-            if (src == srcTemp.length - 1 || dst == dstTemp.length - 1) return;
-                // Добавление нулевого трафика в рядом стоящую ячейку (Невырожденный план)
-            else plan[src][dst + 1].setTraffic(0);
-            CellDistribute(srcTemp, dstTemp, src + 1, dst + 1);
-        }
-        // Потребителю нужно еще груза
-        else {
-            plan[src][dst].setTraffic(srcTemp[src]);
-            dstTemp[dst] -= srcTemp[src];
-            srcTemp[src] = 0;
-            CellDistribute(srcTemp, dstTemp, src + 1, dst);
-        }
+    public List<Integer> getBase() {
+        return base;
+    }
+    public int getTotalCost() {
+        return totalCost;
     }
 }
