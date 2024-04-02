ab713c4f7b02cf117feb90e586b8d7057274469a
duahifnv <max135603@gmail.com>
2024-04-03 00:51:06 +0300

    Добавлен поиск минимальной дельта оценки

---
+++ TransportProblem/src/transport/Optimizer.java
@@ -0,0 +1,93 @@
+package transport;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Класс оптимизации опорного плана
+ */
+public class Optimizer {
+    private Cell worstDelta;
+    private Table table;
+    private Plan plan;
+    private int[] srcPotentials;
+    private int[] dstPotentials;
+    public Optimizer(Plan plan) {
+        this.plan = plan;
+        this.table = plan.getTable();
+    }
+    /** Поиск потенциалов плана */
+    public void FindPotentials() {
+        Cell[][] cells = table.getCells();
+        int[][] costs = table.getCosts();
+        int[] srcPotentials = new int[table.getSrcWeights().length];
+        int[] dstPotentials = new int[table.getDstWeights().length];
+        List<Integer> unassignedSrcIdx =  new ArrayList<>(IntStream.range(1, srcPotentials.length)
+                .boxed().toList());
+        List<Integer> unassignedDstIdx = new ArrayList<>(IntStream.range(0, dstPotentials.length)
+                .boxed().toList());
+        // Проход по строкам таблицы
+        for (int i = 0; i < cells.length; i++) {
+            // Если все потенциалы столцов и потенциал строки найдены -> пропуск
+            if (unassignedDstIdx.size() == 0 && !unassignedSrcIdx.contains(i)) {
+                continue;
+            }
+            // Проход по столбцам таблицы
+            for (int j = 0; j < cells[i].length; j++) {
+                // Если все потенциалы строки и потенциал столбца найдены -> пропуск
+                if (unassignedSrcIdx.size() == 0 && !unassignedDstIdx.contains(j)) {
+                    continue;
+                }
+                // Если в ячейке есть трафик - находим потенциал столбца
+                if (cells[i][j].isHasTraffic()) {
+                    dstPotentials[j] = costs[i][j] - srcPotentials[i];
+                    unassignedDstIdx.remove(Integer.valueOf(j));
+                    // Находим потенциалы ненайденных строк по столбцу
+                    for(int row : List.copyOf(unassignedSrcIdx)) {
+                        if (cells[row][j].isHasTraffic()) {
+                            srcPotentials[row] = costs[row][j] - dstPotentials[j];
+                            unassignedSrcIdx.remove((Integer)row);
+                        }
+                    }
+                }
+            }
+        }
+        this.srcPotentials = srcPotentials;
+        this.dstPotentials = dstPotentials;
+        System.out.println("Потенциалы поставщиков: " + Arrays.toString(srcPotentials));
+        System.out.println("Потенциалы потребителей: " + Arrays.toString(dstPotentials));
+    }
+    /** Поиск минимальной дельта оценки */
+    public int FindMinDelta() {
+        int minDelta = Integer.MAX_VALUE;
+        for(Cell[] cells : table.getCells()) {
+            for(Cell cell : cells) {
+                if(!(cell.isHasTraffic())) {
+                    int delta = cell.getCost() - (srcPotentials[cell.getX()] + dstPotentials[cell.getY()]);
+                    cell.setDelta(delta);
+                    if (delta < minDelta) {
+                        minDelta = delta;
+                        worstDelta = cell;
+                    }
+                }
+            }
+        }
+        System.out.printf("Минимальная дельта оценка: %d (Δ%d%d)%n", minDelta,
+                worstDelta.getX() + 1, worstDelta.getY() + 1);
+        return minDelta;
+    }
+    /** Цикл пересчета плана */
+    public void RecalcPlan() {
+        worstDelta.setTraffic(0);
+        List<Cell> cycle = new ArrayList<>(Collections.singleton(worstDelta));
+        if (CycleFinder.FindCycle(cycle, table.getCells())) {
+            String formatCycle = cycle.stream().map(x -> String.format("x%d%d", x.getX() + 1, x.getY() + 1))
+                    .collect(Collectors.joining("->"));
+            System.out.println("Цикл перестановок: " + formatCycle);
+        }
+    }
+}
