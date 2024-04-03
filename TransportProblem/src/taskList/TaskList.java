86d4d931046a2370689d9974ada84f49050f858c
duahifnv <max135603@gmail.com>
2024-04-03 13:06:14 +0300

    Класс Main превращен в класс TaskList с одноименным пакетом

--- TransportProblem/src/taskList/TaskList.java
+++ TransportProblem/src/taskList/TaskList.java
@@ -1,14 +1,22 @@
+package taskList;
+
 import transport.*;
 
- class Main {
+public class TaskList {
     public static void main(String[] args) {
+        /*int[] srcWeights = {150, 320, 400};
+        int[] dstWeights = {100, 120, 100, 200, 300};
+        int[][] costs = {{1, 2, 4, 1, 5},
+                         {1, 2, 1, 3, 1},
+                         {2, 1, 3, 3, 1}};*/
         int[] srcWeights = {200, 120, 150};
         int[] dstWeights = {100, 90, 200, 30, 80};
         int[][] costs = {{1, 2, 4, 1, 5},
                          {1, 2, 1, 3, 1},
                          {2, 1, 3, 3, 1}};
         Transport transport = new Transport(srcWeights, dstWeights, costs);
-        transport.CreateSourcePlan();
-            transport.PrintTable();
+        transport.PrintTable();
+        transport.PrintTotalCost();
+        transport.Optimize();
     }
 }
