553f3ae0c050b5863c22327d993ce760caabc42f
duahifnv <max135603@gmail.com>
2024-04-15 22:54:19 +0300

    Добавлен метод вычисления оптимальной стоимости

--- MatrixChain/src/MatrixChain.java
+++ MatrixChain/src/MatrixChain.java
@@ -4,8 +4,8 @@
  */
 public class MatrixChain {
     private final int[] vector;
-    private int[][] minScalars;
-    private int[][] optIndices;
+    private Integer[][] minScalars;
+    private Integer[][] optIndices;
     public MatrixChain(Matrix[] matrices) {
         this.vector = new int[matrices.length + 1];
         vector[0] = matrices[0].rows();
@@ -16,8 +16,33 @@ public class MatrixChain {
             vector[i] = matrices[i].rows();
         }
         vector[vector.length - 1] = matrices[matrices.length - 1].cols();
+        minScalars = new Integer[matrices.length][matrices.length];
+        optIndices = new Integer[matrices.length][matrices.length];
     }
     public int[] getVector() {
         return vector;
     }
+    public void calcOptimal() {
+        int n = vector.length - 1;
+        // Fill main diagonals with zeros
+        for (int i = 0; i < n; i++) {
+            minScalars[i][i] = 0;
+            optIndices[i][i] = 0;
+        }
+        for (int sublen = 2; sublen <= n; sublen++) {
+            for (int i = 0; i < n - sublen + 1; i++) {
+                int j = i + sublen - 1;
+                minScalars[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int value = minScalars[i][k] + minScalars[k+1][j]
+                            + vector[i] * vector[k+1] * vector[j+1];
+                    if (value < minScalars[i][j]) {
+                        minScalars[i][j] = value;
+                        optIndices[i][j] = k + 1;
+                    }
+                }
+            }
+        }
+    }
+
 }
