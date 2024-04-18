727dcf3f36b3361d58862dd4133348b67363595c
duahifnv <max135603@gmail.com>
2024-04-18 23:47:23 +0300

    Переименовал класс Matrix в MatrixSize

--- MatrixChain/src/matrixChain/MatrixSize.java
+++ MatrixChain/src/matrixChain/MatrixSize.java
@@ -1,6 +1,6 @@
 package matrixChain;
 
-public record Matrix(int rows, int cols) {
+public record MatrixSize(int rows, int cols) {
     @Override
     public String toString() {
         return "Матрица " + rows + " x " + cols;
