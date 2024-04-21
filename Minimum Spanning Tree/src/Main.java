c019dee6813e354755deb2e11457d2ed1e4baa5d
duahifnv <max135603@gmail.com>
2024-04-21 19:56:41 +0300

    Логика

--- Minimum Spanning Tree/src/Main.java
+++ Minimum Spanning Tree/src/Main.java
@@ -1,4 +1,6 @@
 import graph.*;
+import graph.ostov.Method;
+import graph.ostov.Ostov;
 import json.JSONmanager;
 
 import java.util.List;
@@ -9,5 +11,13 @@ public class Main {
         List<Edge> edges = JSONmanager.parse(jsonPath, Edge.class);
         // TODO: Проверка валидности графа
         Graph graph = new Graph(edges);
+        Graph minOstov;
+        try {
+            minOstov = Ostov.findMinimal(graph, Method.KRASKAL);
+        }
+        catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new Error("Не существует указанного метода");
+        }
     }
 }
