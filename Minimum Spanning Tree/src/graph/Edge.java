095569586b815c3e2577104e636f1528ec179268
duahifnv <max135603@gmail.com>
2024-04-21 21:05:17 +0300

    Добавлен toString

--- Minimum Spanning Tree/src/graph/Edge.java
+++ Minimum Spanning Tree/src/graph/Edge.java
@@ -1,4 +1,8 @@
 package graph;
 
 public record Edge(int from, int to, int weight) {
+    @Override
+    public String toString() {
+        return "Из: " + from + " в: " + to + " с весом " + weight;
+    }
 }
