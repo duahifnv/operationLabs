e8077fed218847b064f3fe38a7b9653c5dfacbd0
duahifnv <max135603@gmail.com>
2024-07-15 13:26:28 +0300

    .

--- PreflowPush/src/graph/PreflowPush.java
+++ PreflowPush/src/graph/PreflowPush.java
@@ -1,5 +1,7 @@
 package graph;
 
+import java.util.Arrays;
+
 public class PreflowPush {
     private int[][] costs, flows;
     private int[] heights, excess;
@@ -10,36 +12,49 @@ public class PreflowPush {
             throw new Error("Граф имеет отрицательные веса");
         }
         n = costs.length;
+        // Инициализация потоков
         flows = new int[n][n];
         for (int i = 1; i < n; i++) {
             flows[0][i] = costs[0][i];
             flows[i][0] = -costs[0][i];
         }
+        // Инициализация высот вершин
         heights = new int[n];
         heights[0] = n;
-        heights[n-1] = 0;
         excess = new int[n];
+        // Инициализация избытков
         for (int i = 1; i < n; i++) {
             excess[i] = flows[0][i];
         }
     }
     // Поиск максимального предпотока
     public int find() {
+        int iteration = 1;
         while (true) {
+            System.out.println("Итерация: " + iteration);
+            System.out.println("Потоки: " + Arrays.deepToString(flows));
+            System.out.println("Высоты вершин: " + Arrays.toString(heights));
+            System.out.println("Избытки вершин: " + Arrays.toString(excess));
             int i;
+            // Находим первую избыточную вершину
             for (i = 1; i < n - 1; i++) {
                 if (excess[i] > 0) break;
             }
+            // Если не нашли -> конец итераций
             if (i == n - 1) break;
             int j;
             for (j = 0; j < n; j++) {
                 if (costs[i][j] - flows[i][j] > 0 &&
                         heights[i] == heights[j] + 1) break;
             }
+            // Если нашли вершину, в которой поток меньше значения и она выше соседней вершины
             if (j < n) push(i, j);
+            // Иначе поднимаем вершину
             else lift(i);
+            iteration++;
         }
         int flow = 0;
+        // Находим максимальный поток как сумму потоков с значением дуги > 0
         for (int i = 0; i < n; i++) {
             if (costs[0][i] > 0) {
                 flow += flows[0][i];
@@ -63,6 +78,7 @@ public class PreflowPush {
         flows[to][from] = -flows[from][to];
         excess[from] -= d;
         excess[to] += d;
+        System.out.println(">Протолкнули предпоток из " + from + " в " + to);
     }
     // Операция поднятия вершины
     private void lift(int vertex) {
@@ -74,5 +90,6 @@ public class PreflowPush {
         }
         if (d == Integer.MAX_VALUE) return;
         heights[vertex] = d + 1;
+        System.out.println(">Подняли вершину " + vertex + " на " + (d + 1));
     }
 }
